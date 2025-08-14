ec93e66f6c9b8b29cf8ee65492204ab084b3c93c
Aabhash Rahut <aabhashrahut00@gmail.com>
2025-08-14 17:33:53 +0000

    command line example

---
+++ AverageOfMarks.java
@@ -0,0 +1,8 @@
+public class AverageOfMarks {
+    public static void main(String[] args) {
+        float avg;
+        avg = 
+        (Float.valueOf(args[0]) + Float.valueOf(args[1]) + Float.valueOf(args[2]) + Float.valueOf(args[3]) + Float.valueOf(args[4]))/5;
+        System.out.println(avg);
+    }
+}
