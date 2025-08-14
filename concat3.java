674f5995d8cce133e9e0bc1f0105d1c70142196b
Aabhash Rahut <aabhashrahut00@gmail.com>
2025-08-14 16:52:56 +0000

    concatenation 3

---
+++ concat3.java
@@ -0,0 +1,8 @@
+public class concat3 {
+    public static void main(String[] args) {
+        char a = 65; //ASCII unicode of 65 is A
+        char b = 'A';
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
