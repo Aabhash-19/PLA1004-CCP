bd0219f1d39eaf89fb6c9f02b28aa82d87058420
Aabhash Rahut <aabhashrahut00@gmail.com>
2025-08-14 16:11:28 +0000

    print statement

---
+++ AssignmentOperator.java
@@ -0,0 +1,5 @@
+public class AssignmentOperator {
+    public static void main(String[] args) {
+        System.out.println("Java programming.");
+    }
+}
--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-public class WelcomeToJava
+public class Main
 {
     public static void main(String[] args)
     {
