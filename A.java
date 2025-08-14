98f761b792c9dff2807e4f573f9d430f291d4fe1
Aabhash Rahut <aabhashrahut00@gmail.com>
2025-08-14 17:23:15 +0000

    command line 2

--- A.java
+++ A.java
@@ -1,5 +1,7 @@
 public class A {
     public static void main(String[] args) {
         System.out.println(args[0] + args[1]); // Concatenates the first two arguments
+
+        System.out.println(Integer.parseInt(args[0]) + Integer.parseInt(args[1])); // Numerical addition
     }
 }
